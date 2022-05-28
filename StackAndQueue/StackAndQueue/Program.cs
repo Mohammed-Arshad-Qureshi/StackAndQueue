129dec130f858031cf42896c1ec0c2ac41bf4168
Arshad <mohammedarshad183@gmail.com>
2022-05-28 15:05:08 +0530

    Ability to create a Stack of 56->30->70

---
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack llStack = new LinkedListStack();
+            llStack.Push(56);
+            llStack.Push(30);
+            llStack.Push(70);
+            llStack.Display();
+
+        }
+    }
+}
